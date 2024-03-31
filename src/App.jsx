da8d4bc26e88408b95779d7da69d2972465c80d5
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-03-31 08:55:42 +0530

    section structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const App = () => {
-  return <div className="bg-slate-50">App</div>;
+  return (
+    <div className="relative">
+      Nav
+      <section className="xl:padding-1 wide:padding-r">hero </section>
+      <section className="padding">Popular products</section>
+      <section className="padding">Super Quality</section>
+      <section className="padding">Services</section>
+      <section className="padding">Special Offer</section>
+      <section className="padding bg-pale-blue">Customer Reviews</section>
+      <section className="padding-x py-16 w-full">Subscribe</section>
+      <section className="padding bg-black padding-x padding-t">Footer</section>
+    </div>
+  );
 };
 
 export default App;
