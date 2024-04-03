2dfd9f86f7d3f248ea4bf3753104544659fc107c
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-04-03 09:29:06 +0530

    shoe card created

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,6 +3,7 @@ import Button from "../components/Button";
 import { arrowRight } from "../assets/icons";
 import { shoes, statistics } from "../constants";
 import { bigShoe1 } from "../assets/images";
+import ShoeCard from "../components/ShoeCard";
 
 const Hero = () => {
   return (
@@ -56,13 +57,13 @@ const Hero = () => {
           height={500}
           className="object-contain relative z-10"
         />
-      </div>
-      <div>
-        {shoes.map((shoe) => (
-          <div key={shoe}>
-            <ShoeCard />
-          </div>
-        ))}
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6"  >
+          {shoes.map((shoe) => (
+            <div key={shoe}>
+              <ShoeCard imgURL={shoe} changeBigShoeImage={() => {}} />
+            </div>
+          ))}
+        </div>
       </div>
     </section>
   );
